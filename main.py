a65249432dda9b55913ad1f275ce0574f8d3fd24
Anuj Gupta <apgupta3091@gmail.com>
2021-09-16 14:01:41 -0400

    Add files

---
+++ main.py
@@ -0,0 +1,43 @@
+print('''
+*******************************************************************************
+          |                   |                  |                     |
+ _________|________________.=""_;=.______________|_____________________|_______
+|                   |  ,-"_,=""     `"=.|                  |
+|___________________|__"=._o`"-._        `"=.______________|___________________
+          |                `"=._o`"=._      _`"=._                     |
+ _________|_____________________:=._o "=._."_.-="'"=.__________________|_______
+|                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
+|___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
+          |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
+ _________|___________| ;`-.o`"=._; ." ` '`."\` . "-._ /_______________|_______
+|                   | |o;    `"-.o`"=._``  '` " ,__.--o;   |
+|___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
+____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
+/______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______/_
+____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
+/______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
+____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
+/______/______/______/______/______/______/______/______/______/______/_____ /
+*******************************************************************************
+''')
+print("Welcome to Treasure Island.")
+print("Your mission is to find the treasure.") 
+
+#https://www.draw.io/?lightbox=1&highlight=0000ff&edit=_blank&layers=1&nav=1&title=Treasure%20Island%20Conditional.drawio#Uhttps%3A%2F%2Fdrive.google.com%2Fuc%3Fid%3D1oDe4ehjWZipYRsVfeAx2HyB7LCQ8_Fvi%26export%3Ddownload
+
+direction = input("You are at a crossroad, would you like to go left or right? type 'left' or 'right':\n")
+
+if direction == "left":
+  choice_two = input("You have arrived at a lake, there is an island in the middle of the lake, type 'swim' to swim to the island or 'wait' to wait for a boat':\n")
+  if choice_two == "wait":
+    choice_three = input("You arrive at the island unharmed. There is a house with 3 doors, One red, One yellow, and One blue. Which color do you choose:\n")
+    if choice_three == "yellow":
+      print("You WIN!")
+    elif choice_three == "red":
+      print("Burned by fire. GAME OVER")
+    else:
+      print("Eaten by beasts. GAME OVER")
+  else:
+    print("You were attacked by a trout. GAME OVER")
+else: 
+  print("You fell into a hole. GAME OVER")
